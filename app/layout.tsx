050fc6bf34cf40116ef776959748924626a53230
rlatkd <rlatkdgns042@naver.com>
2024-11-23 19:13:06 +0900

    feat: posts 초기 마이그레이션

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
-import themeEffect from './utils/themeEffect'
+import { themeEffect } from './utils/themeEffect'
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
