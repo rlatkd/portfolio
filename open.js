c096144da24c149fab2249d8e910195e6ae9d9c0
rlatkd <rlatkdgns042@naver.com>
2024-11-30 10:58:19 +0900

    feat: OS에 따른 open 방식 추가

---
+++ open.js
@@ -0,0 +1,17 @@
+const { exec } = require("child_process");
+
+const url = "http://localhost:3000";
+
+const open = () => {
+  const platform = process.platform;
+
+  if (platform === "darwin") { // macOS
+    exec(`open ${url}`);
+  } else if (platform === "win32") { // Windows
+    exec(`start ${url}`);
+  } else if (platform === "linux") { // Linux
+    exec(`xdg-open ${url}`);
+  }
+};
+
+open();
