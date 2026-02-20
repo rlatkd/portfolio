86c146487cbbefb8b4a2a4caa09ca79d16dba65b
rlatkd <rlatkdgns042@naver.com>
2026-02-20 18:45:40 +0900

    fix: 모바일 중앙 정렬 패딩 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
   return (
     <>
       <section>
-        <div className='max-w-4xl mx-auto'>
+        <div className='max-w-4xl mx-auto px-4 md:px-0'>
           <Hero />
           <Characteristics />
           <Achievement />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,14 +59,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
         />
       </head> */}
       <body className='antialiased max-w-6xl mx-auto px-4 md:px-0 overflow-x-hidden'>
-        <div className='fixed inset-0 bg-gradient-to-r from-black/95 to-gray-900/95 blur-3xl -z-10'></div>
         <main className='flex-auto min-w-0 flex flex-col'>
-          <div>
-            {children}
-          </div>
+          {children}
           <Analytics />
           <SpeedInsights />
-       </main>
+        </main>
       </body>
     </html>
   )
